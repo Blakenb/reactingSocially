ecf1c6dc414aa3b95ef8700b396453308c6e7dc7
Blakenb <blakenb87@gmail.com>
2025-04-17 20:22:06 -0500

    things are starting to work

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,84 +1,80 @@
-import { User } from '../models/index.js';
-import { Request, Response } from 'express';
+import { User } from "../models/index.js";
+import { Request, Response } from "express";
 
-
-  // Get all users
-  export const getUsers = async (_req: Request, res: Response) => {
-    try {
-      const users = await User.find();
-      res.json(users);
-    } catch (err) {
-      res.status(500).json(err);
-    }
+// Get all users
+export const getUsers = async (_req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
   }
+};
 
-  // Get a single user
-  export const getSingleUser = async (req: Request, res: Response) => {
-    try {
-      const user = await User.findOne({ _id: req.params.userId })
-        .select('-__v');
+// Get a single user
+export const getSingleUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOne({ _id: req.params.userId }).select("-__v");
 
-      if (!user) {
-        return res.status(404).json({ message: 'No user with that ID' });
-      }
-
-      res.json(user);
-      return;
-    } catch (err) {
-      res.status(500).json(err);
-      return;
+    if (!user) {
+      return res.status(404).json({ message: "No user with that ID" });
     }
-  }
 
-  // create a new user
-  export const createUser = async (req: Request, res: Response) => {
-    try {
-      const user = await User.create(req.body);
-      res.json(user);
-    } catch (err) {
-      res.status(500).json(err);
+    res.json(user);
+    return;
+  } catch (err) {
+    res.status(500).json(err);
+    return;
+  }
+};
+
+// create a new user
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// update a user and associated apps
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOneAndUpdate({ _id: req.params.userId });
+    if (!user) {
+      return res.status(404).json({ message: "No user with that ID" });
     }
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { new: true }
+    );
+    res.json(updatedUser);
+    return;
+  } catch (err) {
+    res.status(500).json(err);
+    return;
   }
+};
 
-  // update a user and associated apps
-  export const updateUser = async (req: Request, res: Response) => {
-    try {
-      const user = await User.findOneAndUpdate(
-        { _id: req.params.userId });
-      if (!user) {
-        return res.status(404).json({ message: 'No user with that ID' });
-      }
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: req.params.userId },
-        { $set: req.body },
-        { new: true }
-      );
-      res.json(updatedUser);
-      return;
-    } catch (err) {
-      res.status(500).json(err);
-      return;
+// Delete a user and associated apps
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+    if (!user) {
+      return res.status(404).json({ message: "No user with that ID" });
     }
+
+    await User.deleteMany({ _id: { $in: user } });
+    res.json({ message: "User and associated apps deleted!" });
+    return;
+  } catch (err) {
+    res.status(500).json(err);
+    return;
   }
-
-  // Delete a user and associated apps
-  export const deleteUser = async (req: Request, res: Response) => {
-    try {
-      const user = await User.findOneAndDelete({ _id: req.params.userId });
-
-      if (!user) {
-        return res.status(404).json({ message: 'No user with that ID' });
-      }
-
-      await User.deleteMany({ _id: { $in: user } });
-      res.json({ message: 'User and associated apps deleted!' })
-      return;
-    } catch (err) {
-      res.status(500).json(err);
-      return;
-    }
-  }
-
+};
 
 export const addFriend = async (req: Request, res: Response) => {
   const { userId, friendId } = req.params;
@@ -94,21 +90,19 @@ export const addFriend = async (req: Request, res: Response) => {
     );
     // validate if the user exists
     if (!user) {
-      return res.status(404).json({ message: 'No user with that ID' });
+      return res.status(404).json({ message: "No user with that ID" });
     }
     return res.json(user);
   } catch (error) {
-    return res.status(500).json({ message: 'Error adding friend' });
+    return res.status(500).json({ message: "Error adding friend" });
   }
 };
-
-
 
 export const removeFriend = async (req: Request, res: Response) => {
   const { userId, friendId } = req.params;
   // Check if the user exists
   // We can REMOVE the friendId value from the current uer's friends array
-  try { 
+  try {
     const user = await User.findOneAndUpdate(
       { _id: userId },
       { $pull: { friends: friendId } },
@@ -116,25 +110,26 @@ export const removeFriend = async (req: Request, res: Response) => {
     );
     // validate if the user exists
     if (!user) {
-      return res.status(404).json({ message: 'No user with that ID' });
+      return res.status(404).json({ message: "No user with that ID" });
     }
     return res.json(user);
   } catch (error) {
-    return res.status(500).json({ message: 'Error removing friend' });
-  }};
+    return res.status(500).json({ message: "Error removing friend" });
+  }
+};
 
 export const getFriends = async (req: Request, res: Response) => {
   const { userId } = req.params;
 
   try {
-    const user = await User.findById(userId).populate('friends');
+    const user = await User.findById(userId).populate("friends");
 
     if (!user) {
-      return res.status(404).json({ message: 'No user with that ID' });
+      return res.status(404).json({ message: "No user with that ID" });
     }
 
     return res.json(user.friends);
   } catch (error) {
-    return res.status(500).json({ message: 'Error retrieving friends' });
+    return res.status(500).json({ message: "Error retrieving friends" });
   }
-}
+};
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import db from "./config/connections.js";
+import routes from "./routes/index.js";
+
+const cwd = process.cwd();
+
+const PORT = 3001;
+const app = express();
+
+// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
+// These next two liners PARSE the incoming requests with urlencoded payloads and JSON payloads
+// and make them available under the req.body property
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+// we need to connect to our database before starting the Express server
+db.once("open", () => {
+  console.log(`Connected to database for ${cwd}`);
+  app.listen(PORT, () => {
+    console.log(`API server for ${cwd} running on port ${PORT}!`);
+  });
+});
