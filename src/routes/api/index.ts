569ba69b2e6182d27aaf8f7f42a530a2305b160a
Blakenb <blakenb87@gmail.com>
2025-04-17 19:47:04 -0500

    getting the files all setup

---
+++ src/routes/api/index.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+const router = Router();
+// Import API routes
+import thoughtRoutes from "./thoughtRoutes.js";
+import userRoutes from "./userRoutes.js";
+
+// ALL ROUTES are prefixed with '/api'
+router.use("/users", userRoutes);
+router.use("/thoughts", thoughtRoutes);
+
+export default router;
---
+++ src/routes/api/userRoutes.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+const router = Router();
+import {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+  getFriends,
+} from "../../controllers/userController.js";
+
+// /api/users
+router.route("/").get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route("/:userId").get(getSingleUser);
+router.route("/:userId").delete(deleteUser);
+router.route("/:userId").put(updateUser);
+
+// /api/users/:userId/friends
+router.route("/:userId/friends").get(getFriends);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+export default router;
